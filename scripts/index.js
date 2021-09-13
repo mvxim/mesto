329eba208af4de8d94a2681b60a431ad8b6a847e
Maxim Donika <donika.maxim@icloud.com>
2021-09-13 16:35:24 +0300

    Create scripts to make the modal window alive

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+// выбираю элементы на странице
+const modal = document.querySelector('.modal') //выбираем модалку с затемнением
+const profileEditBtn = document.querySelector('.profile__edit-btn') //выбираем кнопку редактирования
+const modalCloseBtn = document.querySelector('.modal__close-btn') //выбираем кнопку закрытия
+const profileName = document.querySelector('.profile__name') //выбираем имя профиля
+const profileDesc = document.querySelector('.profile__desc') //выбираем описание профиля
+const formElement = document.querySelector('.modal__form') //выбираем форму целиком
+const nameInput = document.querySelector('.modal__input_name') //поле ввода имени в форме
+const descInput = document.querySelector('.modal__input_desc') //поле ввода описания в форме
+
+// функции, управляющие открытием и закрытием модалки
+function modalOn() {
+    modal.classList.add('modal_active')
+}
+
+function modalOff() {
+    modal.classList.remove('modal_active')
+}
+
+// листнеры кликов по кнопкам
+profileEditBtn.addEventListener('click', modalOn)
+modalCloseBtn.addEventListener('click', modalOff)
+modal.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
+        modalOff()
+    }
+})
+
+// функция, которая получает value полей формы
+function updateProfileDetails(event) {
+    event.preventDefault() //предотвращаю перезагрузку страницы при отправке формы
+    profileName.textContent = nameInput.value // передаю значение поля name в профиль, в элемент с классом .profile__name
+    profileDesc.textContent = descInput.value // передаю значение поля desc в профиль, в элемент с классом .profile__desc
+    modalOff()
+}
+
+formElement.addEventListener('submit', updateProfileDetails)
