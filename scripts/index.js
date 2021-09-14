3e641ff646c6864df10e596f3e143aa876b3ae59
Maxim Donika <donika.maxim@icloud.com>
2021-09-14 13:12:09 +0300

    feat: create like button logic

--- scripts/index.js
+++ scripts/index.js
@@ -7,42 +7,51 @@ const profileDesc = document.querySelector('.profile__desc') //выбираем 
 const formElement = document.querySelector('.modal__form') //выбираем форму целиком
 const nameInput = document.querySelector('.modal__input_name') //поле ввода имени в форме
 const descInput = document.querySelector('.modal__input_desc') //поле ввода описания в форме
+const likeBtn = document.querySelectorAll('.like') //все кнопки лайка
 
 // открывает модальное окно
 function modalOn() {
-    modal.classList.add('modal_active')
-    page.classList.add('page_no-scroll')
-    nameInput.value = profileName.textContent
-    descInput.value = profileDesc.textContent
+  modal.classList.add('modal_active')
+  page.classList.add('page_no-scroll')
+  nameInput.value = profileName.textContent
+  descInput.value = profileDesc.textContent
 }
 
 // закрывает модальное окно
 function modalOff() {
-    modal.classList.remove('modal_active')
-    page.classList.remove('page_no-scroll')
+  modal.classList.remove('modal_active')
+  page.classList.remove('page_no-scroll')
 }
 
 // обновляет текст на странице в разделе «профиль»
 function updateProfileDetails(event) {
-    event.preventDefault() //предотвращает перезагрузку страницы при отправке формы
-    if (nameInput.value === '') { //если в поле формы ничего не ввели, то ничего не происходит
-    } else {
-        profileName.textContent = nameInput.value //если всё-таки ввели, то значение из поля подставляется в страницу
-    }
-    if (descInput.value === '') {
-    } else {
-        profileDesc.textContent = descInput.value
-    }
-    modalOff() // при сохранении модалка закрывается
+  event.preventDefault() //предотвращает перезагрузку страницы при отправке формы
+  if (nameInput.value === '') { //если в поле формы ничего не ввели, то ничего не происходит
+  } else {
+    profileName.textContent = nameInput.value //если всё-таки ввели, то значение из поля подставляется в страницу
+  }
+  if (descInput.value === '') {
+  } else {
+    profileDesc.textContent = descInput.value
+  }
+  modalOff() // при сохранении модалка закрывается
 }
 
 // листнеры событий
 profileEditBtn.addEventListener('click', modalOn) // при клике по кнопке редактирования открывает модалку
 modalCloseBtn.addEventListener('click', modalOff) // при клике по кнопке закрытия закрывает
 modal.addEventListener('click', function (event) { // при клике за пределами формы, но в пределах оверлея закрывает модалку
-    if (event.target === event.currentTarget) {
-        modalOff()
-    }
+  if (event.target === event.currentTarget) {
+    modalOff()
+  }
 })
 
+// отправка формы
 formElement.addEventListener('submit', updateProfileDetails) // когда форма отправляется, обновляет текст на странице
+
+// активация лайка
+for (let button = 0; button < likeBtn.length; button++) { // вешаем листнер на каждый элемент массива с классом .like
+  likeBtn[button].addEventListener('click', function (event) {
+    event.target.classList.toggle('like_active') // элемент, по которому кликнули, меняет свой класс
+  })
+}
