84106e89431b23b396e57c01b9fa0a78a2c1ca6d
Maxim Donika <donika.maxim@icloud.com>
2021-09-30 18:33:28 +0300

    feat: add ability to remove any place card

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ const galleryTemplateItem = document.querySelector('.gallery__item-template')
 const profileName = document.querySelector('.profile__name')
 const profileDesc = document.querySelector('.profile__desc')
 
-// Модалки
+// Фон модалок
 const modal = document.querySelectorAll('.modal')
 
 // Редактирование био
@@ -26,7 +26,7 @@ const pictureInput = formCardElement.querySelector('.modal__input_field_picture'
 const profileEditBtn = document.querySelector('.profile__edit-btn')
 const addNewPictureBtn = document.querySelector('.profile__add-btn')
 const modalCloseBtn = document.querySelectorAll('.modal__close-btn')
-const deleteItemBtn = document.querySelectorAll('.gallery__remove-btn')
+const deleteItemBtn = document.querySelectorAll('.gallery__delete-btn')
 
 // Набор карточек
 const places = [
@@ -56,18 +56,19 @@ const places = [
   }
 ];
 
+// создает карточки с местами
 function renderPlace(place) {
   const newPlace = galleryTemplateItem.content.cloneNode(true)
   newPlace.querySelector('.gallery__text').textContent = place.name
   newPlace.querySelector('.gallery__image').setAttribute('src', place.link)
   newPlace.querySelector('.like').addEventListener('click', like)
+  newPlace.querySelector('.gallery__delete-btn').addEventListener('click', deletePlace)
   gallery.prepend(newPlace)
 }
 
 places.forEach(renderPlace)
 
-
-// открывает модальное окно
+// открывает модальные окна
 function modalOn(modalElement) {
   modalElement.classList.add('modal_active')
   if (modalElement === formBioElement) {
@@ -77,46 +78,15 @@ function modalOn(modalElement) {
   page.classList.add('page_no-scroll')
 }
 
-// закрывает модальное окно
+profileEditBtn.addEventListener('click', () => modalOn(formBioElement))
+addNewPictureBtn.addEventListener('click', () => modalOn(formCardElement))
+
+// закрывает модальные окна
 function modalOff(modalElement) {
   modalElement.classList.remove('modal_active')
   page.classList.remove('page_no-scroll')
 }
 
-// обновляет текст на странице в разделе «профиль»
-function updateProfileDetails(event) {
-  event.preventDefault()
-  if (nameInput.value === '') {
-  } else {
-    profileName.textContent = nameInput.value
-  }
-  if (descInput.value === '') {
-  } else {
-    profileDesc.textContent = descInput.value
-  }
-  modalOff(formBioElement)
-}
-formBioElement.addEventListener('submit', updateProfileDetails)
-
-// добавляет новую карточку
-function createNewPlace(event) {
-  event.preventDefault()
-  let newItem = {
-    name: titleInput.value,
-    link: pictureInput.value,
-  }
-  places.push(newItem)
-  renderPlace(newItem)
-  modalOff(formCardElement)
-}
-formCardElement.addEventListener('submit', createNewPlace)
-
-// Листнеры: открытие модальных окон
-profileEditBtn.addEventListener('click', () => modalOn(formBioElement))
-addNewPictureBtn.addEventListener('click', () => modalOn(formCardElement))
-
-
-// Листнеры: закрытие модальных окон
 modal.forEach((item) => {
   item.addEventListener('click', (event) => {
     if (event.target === event.currentTarget) {
@@ -131,12 +101,43 @@ modalCloseBtn.forEach((item) => {
   })
 })
 
+// обновляет текст на странице в разделе «профиль»
+function updateProfileDetails(event) {
+  event.preventDefault()
+  if (nameInput.value === '') {
+  } else {
+    profileName.textContent = nameInput.value
+  }
+  if (descInput.value === '') {
+  } else {
+    profileDesc.textContent = descInput.value
+  }
+  modalOff(formBioElement)
+}
 
-// Манипуляции с элементами: обновление инфы в био и добавление нового места
- // когда форма отправляется, обновляет текст на странице
+formBioElement.addEventListener('submit', updateProfileDetails)
 
+// добавляет новую карточку
+function createNewPlace(event) {
+  event.preventDefault()
+  let newItem = {
+    name: titleInput.value,
+    link: pictureInput.value,
+  }
+  places.push(newItem)
+  renderPlace(newItem)
+  modalOff(formCardElement)
+}
 
-// активация лайка
+formCardElement.addEventListener('submit', createNewPlace)
+
+// ставит лайк
 function like(event) {
   event.target.classList.toggle('like_active')
+}
+
+// удаляет карточку
+function deletePlace(event) {
+  const place = event.currentTarget.closest('.gallery__item')
+  place.remove()
 }
