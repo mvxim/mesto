ad5a4b2842d6fefa14436d6665e9f3e1dada2de8
Maxim Donika <donika.maxim@icloud.com>
2021-11-08 16:19:49 +0300

    refactor: configure webpack

---
+++ src/index.js
@@ -0,0 +1,108 @@
+// картинки импортируются в ./utils/constants.js
+import "./pages/index.css"
+
+import {
+  galleryContainerSelector,
+  galleryItemTemplate,
+  nameInput,
+  descInput,
+  profileEditBtn,
+  addNewPictureBtn,
+  bioModalSelector,
+  cardModalSelector,
+  maxModalSelector,
+  profileNameSelector,
+  profileDescSelector,
+  places
+} from "./utils/constants.js"
+import { Section } from "./components/Section.js"
+import { Card } from "./components/Card.js"
+import {
+  formConfig,
+  FormValidator,
+} from "./components/FormValidator.js"
+import { PopupWithImage } from "./components/PopupWithImage.js"
+import { PopupWithForm } from "./components/PopupWithForm.js"
+import { UserInfo } from "./components/UserInfo.js"
+
+// ***************************************************************************
+// управление именем и информацией
+
+// экземпляр модалки с формой био
+const bioModal = new PopupWithForm(bioModalSelector, (inputValues) => {
+  bioData.setUserInfo(inputValues)
+  bioModal.close()
+})
+
+// экземпляр UserInfo для управления разделом био на странице
+const bioData = new UserInfo({
+  nameSelector: profileNameSelector,
+  infoSelector: profileDescSelector,
+})
+
+// обработка нажатия на кнопку «Редактировать»
+profileEditBtn.addEventListener("mousedown", () => {
+  const currentBioData = bioData.getUserInfo()
+  nameInput.value = currentBioData.name
+  descInput.value = currentBioData.info
+  bioModal.open()
+})
+
+// ***************************************************************************
+//управление отображением и добавлением карточек
+
+// экземпляр модалки с картинкой
+const maxModal = new PopupWithImage(maxModalSelector)
+
+// функция-рендерер-карточек, для использования к колбеке конструктора Section
+const renderNewCard = (place) => {
+  const newPlace = new Card({
+    data: place,
+    handleCardClick: () => {
+      maxModal.open(place)
+    }
+  }, galleryItemTemplate)
+  const newPlaceElement = newPlace.assembleCard()
+  gallerySection.addItem(newPlaceElement)
+}
+
+// экземпляр Section, который рисует карточки при загрузке страницы
+const gallerySection = new Section({
+  data: places, renderer: (place) => {
+    renderNewCard(place)
+  }
+}, galleryContainerSelector)
+
+gallerySection.renderItems()
+
+// экземпляр модалки с формой карточки
+const cardModal = new PopupWithForm(cardModalSelector, (inputValues) => {
+  const newItem = [ {
+    name: inputValues["card-field-title"],
+    link: inputValues["card-field-picture"],
+  } ]
+  const newCard = new Section({
+    data: newItem, renderer: (place) => {
+      renderNewCard(place)
+    }
+  })
+  newCard.renderItems()
+  cardModal.close()
+})
+
+// обработка нажатия на кнопку «Добавить»
+addNewPictureBtn.addEventListener("mousedown", () => {
+  cardModal.open()
+})
+
+// ***************************************************************************
+// управление валидацией для всех форм
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector))
+  formList.forEach((formElement) => {
+    const formToValidate = new FormValidator(formElement, config)
+    formToValidate.enableValidation()
+  })
+}
+
+enableValidation(formConfig)
---
+++ src/utils/constants.js
@@ -0,0 +1,61 @@
+import zubchatki from "../images/gallery/1.jpg"
+import kurum from "../images/gallery/2.jpg"
+import dzhabyk from "../images/gallery/3.jpg"
+import agidel from "../images/gallery/4.jpg"
+import jamantau from "../images/gallery/5.jpg"
+import turgojak from "../images/gallery/6.jpg"
+
+export const places = [
+  {
+    name: "💙 Зубчатки",
+    link: zubchatki,
+  },
+  {
+    name: "🗿 Курум",
+    link: kurum,
+  },
+  {
+    name: "🏝 Джабык",
+    link: dzhabyk,
+  },
+  {
+    name: "🏞 Река Агидель",
+    link: agidel,
+  },
+  {
+    name: "🏔 Ямантау",
+    link: jamantau,
+  },
+  {
+    name: "🌊 Тургояк",
+    link: turgojak,
+  },
+]
+
+
+// Страница
+export const page = document.querySelector(".page")
+
+// Галерея
+export const galleryItemTemplate = ".gallery__item-template"
+export const galleryContainerSelector = ".gallery__grid"
+
+// Селекторы имени и описания
+export const profileNameSelector = ".profile__name"
+export const profileDescSelector = ".profile__desc"
+
+// Редактирование био
+export const formBioElement = document.querySelector(".modal_type_bio")
+export const bioModalSelector = ".modal_type_bio"
+export const nameInput = formBioElement.querySelector(".modal__input_field_name")
+export const descInput = formBioElement.querySelector(".modal__input_field_desc")
+
+// Добавление карточки
+export const cardModalSelector = ".modal_type_card"
+
+// Открытие карточки на весь экран
+export const maxModalSelector = ".modal_type_picture"
+
+// Кнопки
+export const profileEditBtn = document.querySelector(".profile__edit-btn")
+export const addNewPictureBtn = document.querySelector(".profile__add-btn")
